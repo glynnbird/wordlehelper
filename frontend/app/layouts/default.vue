2626ec7c892a62b5a21f00e3a2904587ba7c0ab7
Glynn Bird <glynn.bird@gmail.com>
2026-05-08 13:25:21 +0100

    better styling

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -1,24 +1,10 @@
 <script setup>
-   const { search } = useWordList()
+
 </script>
-<style>
-.inputword {
-font-family: monospace, monospace; 
-}
-</style>
 <template>
   <v-app theme="light">
-    <v-app-bar density="compact" extended extension-height="70">
+    <v-app-bar density="compact">
       <v-app-bar-title @click="navigateTo('/')" style="user-select:none;">Wordle Helper</v-app-bar-title>
-      <template v-slot:extension>
-        <v-row>
-          <v-col cols="1"></v-col>
-          <v-col cols="10">
-             <v-text-field class="inputword" maxlength="5" clearable placeholder="Search" v-model="search"></v-text-field>
-          </v-col>
-          <v-col cols="1"></v-col>
-        </v-row>
-      </template>
     </v-app-bar>
     <v-main>
       <v-container fluid>
--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,12 +1,18 @@
 <script setup>
   // composables
   const { $pwa } = useNuxtApp()
-  const { filteredWords } = useWordList()
+  const { filteredWords, search } = useWordList()
 </script>
 <style>
   .v-card {
     margin-bottom: 10px;
   }
+.inputword {
+  font-family: monospace, monospace; 
+}
+.inputword input {
+  font-size: 40px;
+}
 </style>
 <template>
   <!-- PWA refresh banner-->
@@ -14,6 +20,14 @@
     <h4> New content available, click on reload button to update. </h4>
     <v-btn color="primary" @click="$pwa.updateServiceWorker()">Reload</v-btn>
   </v-alert>
+
+  <v-row>
+    <v-col cols="1"></v-col>
+    <v-col cols="10">
+      <v-text-field class="inputword" maxlength="5" persistent-counter  counter clearable placeholder="Search" v-model="search"></v-text-field>
+    </v-col>
+    <v-col cols="1"></v-col>
+  </v-row>
 
   <v-list>
     <v-list-item v-for="word in filteredWords">{{ word }}</v-list-item>
